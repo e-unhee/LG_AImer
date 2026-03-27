36b789218e1d0b1560f3852a5f4ce1cde88ce1c2
e-unhee <dkfzhd1509@g.skku.edu>
2026-03-27 23:26:41 +0900

    [fix]타일 사이즈 배당 조건 변경

--- vllm/model_executor/layers/quantization/compressed_tensors/triton_scaled_mm.py
+++ vllm/model_executor/layers/quantization/compressed_tensors/triton_scaled_mm.py
@@ -204,7 +204,7 @@ def triton_scaled_mm(
 
     # 커스터마이징 2
     # 휴리스틱 때문에 작은 타일 사이즈 배당 -> 무시하고 큰 사이즈로
-    tile_shape = (128, 256, 128)
+    tile_shape = (128, 128, 256)
 
     block_size_m, block_size_n, block_size_k = tile_shape
 
